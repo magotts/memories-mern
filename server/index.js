c35e6a57a8c78c5c4e6097e10d6e5db7d658ef15
magotts <techybet@gmail.com>
2021-11-30 11:35:12 -0500

    add reducer

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({limit: "30m", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30m", extended: true}));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 // CONNECT TO MongoDB
 const CONNECTION_URL = "mongodb+srv://admin:admin@cluster0.32oec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
